04d40b92e71f382d32dd1e0e28a0038fda46b6ad
Johanna Lee <johanna.lee@rangle.io>
2019-05-03 16:35:45 -0400

    flatten out one level of folder structure

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,7 @@
 import { configure } from '@storybook/angular';
 
 // automatically import all files ending in *.stories.ts
-const req = require.context('../projects/lib/', true, /\.stories\.ts$/);
+const req = require.context('../lib/', true, /\.stories\.ts$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
