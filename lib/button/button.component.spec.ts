1af5edd8b571b2862f30c946a455b1041af3e019
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2019-05-07 15:19:49 -0400

    Fixed issue with snapshot test taken before loading story

--- lib/button/button.component.spec.ts
+++ lib/button/button.component.spec.ts
@@ -1,24 +1,49 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { ButtonComponent } from './button.component';
+import { ButtonComponent } from "./button.component";
 
-describe('LibComponent', () => {
-  let component: ButtonComponent;
-  let fixture: ComponentFixture<ButtonComponent>;
+import * as puppeteer from "puppeteer";
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ButtonComponent]
-    }).compileComponents();
-  }));
+describe("LibComponent", () => {
+  describe("DOM", () => {
+    let component: ButtonComponent;
+    let fixture: ComponentFixture<ButtonComponent>;
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ButtonComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        declarations: [ButtonComponent]
+      }).compileComponents();
+    }));
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(ButtonComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+
+    it("should create", () => {
+      expect(component).toBeTruthy();
+    });
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe("Image Snapshot", () => {
+    let browser;
+
+    beforeEach(async () => {
+      browser = await puppeteer.launch();
+    });
+
+    it("default", async () => {
+      const page = await browser.newPage();
+      await page.goto("http://localhost:6006/?path=/story/button--with-text");
+      await page.waitForSelector('#button--with-text');
+      const image = await page.screenshot();
+
+      expect(image).toMatchImageSnapshot();
+    });
+
+    afterAll(async () => {
+      await browser.close();
+    });
   });
 });
