f5131caa0ba0745cd464c7e9ef662be222db7f23
Johanna Lee <johannalee@users.noreply.github.com>
2019-05-09 17:05:44 -0400

    Add @storybook/addon-notes (#4)

--- lib/button/button.stories.ts
+++ lib/button/button.stories.ts
@@ -1,10 +1,15 @@
 import { storiesOf } from '@storybook/angular';
 import { ButtonComponent } from './button.component';
+import markdown from './README.md';
 
-storiesOf('Button', module).add('with text', () => ({
-  component: ButtonComponent,
-  props: {
-    placeholder: 'Hello Button',
-    classname: 'red'
-  }
-}));
+storiesOf('Button', module).add(
+  'with text',
+  () => ({
+    component: ButtonComponent,
+    props: {
+      placeholder: 'Hello Button',
+      classname: 'green'
+    }
+  }),
+  { notes: { markdown } }
+);
