b037b495f2db8f90472e896fc9988b5b49c21fca
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2019-05-09 17:12:31 -0400

    Added addon-jest

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,12 +1,18 @@
 import { configure, addDecorator } from '@storybook/angular';
 import requireContext from 'require-context.macro';
 
+import { withTests } from '@storybook/addon-jest';
+
+import results from '../test-results.json';
+
 // automatically import all files ending in *.stories.ts
 const req = requireContext('../lib/', true, /\.stories\.ts$/);
+
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
 
 addDecorator(withNotes);
+addDecorator(withTests({ results, filesExt: '((\\.component))((\\.specs?)|(\\.tests?))?(\\.ts)?$' }));
 
 configure(loadStories, module);
