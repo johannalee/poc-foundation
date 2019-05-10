dd0544e234cab12ea8b6df913b1f3f6b8b9ca4a7
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2019-05-10 15:31:43 -0400

    Run prettier

--- lib/button/button.stories.ts
+++ lib/button/button.stories.ts
@@ -1,24 +1,30 @@
-import { storiesOf } from "@storybook/angular";
-import { ButtonComponent } from "./button.component";
-import { text, select } from "@storybook/addon-knobs";
+import { storiesOf } from '@storybook/angular';
+import { ButtonComponent } from './button.component';
+import { text, select } from '@storybook/addon-knobs';
 
 import markdown from './README.md';
 
-storiesOf("Button", module)
+storiesOf('Button', module)
   .addParameters({
     jest: ['button']
   })
-.add("with text", () => ({
-  component: ButtonComponent,
-  props: {
-    placeholder: text("placeholder", "Hello Button"),
-    classname: select("classname", {
-      Red: "red",
-      Green: "green",
-      Blue: "blue"
-    }, 'red')
-  }
-}),
+  .add(
+    'with text',
+    () => ({
+      component: ButtonComponent,
+      props: {
+        placeholder: text('placeholder', 'Hello Button'),
+        classname: select(
+          'classname',
+          {
+            Red: 'red',
+            Green: 'green',
+            Blue: 'blue'
+          },
+          'red'
+        )
+      }
+    }),
 
-  { notes: { markdown } }
-);
+    { notes: { markdown } }
+  );
