c6b3374ef31662b750cf89ca7c83fde2147cf5db
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2019-05-13 10:19:21 -0400

    Loading the iframe path

--- e2e/tests/button.e2e.js
+++ e2e/tests/button.e2e.js
@@ -1,12 +1,11 @@
 import { Selector } from 'testcafe'; // first import testcafe selectors
 
 fixture `ButtonComponent`// declare the fixture
-    .page `http://localhost:6006/?path=/story/button--with-text`;  // specify the start page
+    .page `http://localhost:6006/iframe.html?id=button--with-text`;  // specify the start page
 
 
 //then create a test and place your code there
 test('Page Loads', async t => {
-    await t.switchToIframe('#storybook-preview-iframe');
     await t
-        .expect(Selector('.red').innerText).eql('Hello Button');
+        .expect(Selector('.bc-red-light').innerText).eql('Hello Button');
 });
