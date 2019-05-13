1dceaedfa1c57e4200ced79bedfe3e51f3276484
Aldrin Jerome Almacin <aalmacin@users.noreply.github.com>
2019-05-13 12:22:14 -0400

    Added scss setup (#3)
    * Added scss setup
    
    * Run prettier
    
    * Fixed issue with MD
    
    * Fixed issue with missing scss files
    
    * Added fix for knobs breaking jest test

--- .storybook/config.js
+++ .storybook/config.js
@@ -5,6 +5,10 @@ import { withTests } from '@storybook/addon-jest';
 
 import results from '../test-results.json';
 
+import { withNotes } from '@storybook/addon-notes';
+import { withKnobs } from '@storybook/addon-knobs';
+import { withA11y } from '@storybook/addon-a11y';
+
 // automatically import all files ending in *.stories.ts
 const req = requireContext('../lib/', true, /\.stories\.ts$/);
 
@@ -14,5 +18,7 @@ function loadStories() {
 
 addDecorator(withNotes);
 addDecorator(withTests({ results, filesExt: '((\\.component))((\\.specs?)|(\\.tests?))?(\\.js)?$' }));
+addDecorator(withKnobs)
+addDecorator(withA11y)
 
 configure(loadStories, module);
--- lib/button/button.stories.ts
+++ lib/button/button.stories.ts
@@ -1,6 +1,27 @@
+import { select, text } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/angular';
 import { ButtonComponent } from './button.component';
+
 import markdown from './README.md';
+
+const props =
+  process.env.NODE_ENV === 'test'
+    ? {
+        placeholder: 'Hello Button',
+        classname: 'red'
+      }
+    : {
+        placeholder: text('placeholder', 'Hello Button'),
+        classname: select(
+          'classname',
+          {
+            Red: 'red',
+            Green: 'green',
+            Blue: 'blue'
+          },
+          'red'
+        )
+      };
 
 storiesOf('Button', module)
   .addParameters({
@@ -10,10 +31,7 @@ storiesOf('Button', module)
     'with text',
     () => ({
       component: ButtonComponent,
-      props: {
-        placeholder: 'Hello Button',
-        classname: 'red'
-      }
+      props
     }),
     { notes: { markdown } }
   );
