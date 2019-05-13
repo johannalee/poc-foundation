6f20d70dbf45ccdb2a5dbaf2af6bbbec93b2d5a6
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2019-05-13 14:59:38 -0400

    Fixed issue with knobs not working

--- lib/button/button.stories.ts
+++ lib/button/button.stories.ts
@@ -4,25 +4,6 @@ import { ButtonComponent } from './button.component';
 
 import markdown from './README.md';
 
-const props =
-  process.env.NODE_ENV === 'test'
-    ? {
-        placeholder: 'Hello Button',
-        classname: 'red'
-      }
-    : {
-        placeholder: text('placeholder', 'Hello Button'),
-        classname: select(
-          'classname',
-          {
-            Red: 'red',
-            Green: 'green',
-            Blue: 'blue'
-          },
-          'red'
-        )
-      };
-
 storiesOf('Button', module)
   .addParameters({
     jest: ['button']
@@ -31,7 +12,24 @@ storiesOf('Button', module)
     'with text',
     () => ({
       component: ButtonComponent,
-      props
+      props:
+        process.env.NODE_ENV === 'test'
+          ? {
+              placeholder: 'Hello Button',
+              classname: 'red'
+            }
+          : {
+              placeholder: text('placeholder', 'Hello Button'),
+              classname: select(
+                'classname',
+                {
+                  Red: 'red',
+                  Green: 'green',
+                  Blue: 'blue'
+                },
+                'red'
+              )
+            }
     }),
     { notes: { markdown } }
   );
